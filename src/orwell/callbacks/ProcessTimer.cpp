8ab551b946170bb8d3a2b06bb23bdeaefc818298
dchilot <dchilot@hotmail.com>
2017-02-18 09:37:21 +0000

    WIP: proof of concept - send input message to stop robots when a winner is found.

--- src/orwell/callbacks/ProcessTimer.cpp
+++ src/orwell/callbacks/ProcessTimer.cpp
@@ -4,10 +4,12 @@
 #include "orwell/com/RawMessage.hpp"
 #include "orwell/game/Game.hpp"
 #include "orwell/game/Item.hpp"
+#include "orwell/game/Robot.hpp"
 #include "orwell/game/ItemEncoder.hpp"
 #include "orwell/game/Landmark.hpp"
 #include "orwell/com/Sender.hpp"
 
+#include "controller.pb.h"
 #include "server-game.pb.h"
 
 using orwell::com::RawMessage;
@@ -52,6 +54,21 @@ void ProcessTimer::execute()
 	if (m_game->getWinner())
 	{
 		aGameState.set_winner(*m_game->getWinner());
+		std::map< std::string, std::shared_ptr< orwell::game::Robot > > aRobots =
+			m_game->getRobots();
+		for (auto const & aPair : aRobots)
+		{
+			std::string aRobotRoutingId = aPair.first;
+			auto aRobot = aPair.second;
+			ORWELL_LOG_INFO("Ask robot " << aRobot->getName() << " to stop moving");
+			orwell::messages::Input aInput;
+			aInput.mutable_move()->set_left(0);
+			aInput.mutable_move()->set_right(0);
+			aInput.mutable_fire()->set_weapon1(false);
+			aInput.mutable_fire()->set_weapon2(false);
+			RawMessage aInpurMessage(aRobotRoutingId, "Input", aInput.SerializeAsString());
+			m_publisher->send(aInpurMessage);
+		}
 	}
 
 	RawMessage aMessage("all_clients", "GameState", aGameState.SerializeAsString());
